2b46bfd23a087fbba037d9f85309d39e2aca9ac6
Sen <noppongsen@gmail.com>
2026-01-11 13:53:11 +0700

    พิ่ม Logic เติม http:// อัตโนมัติถ้า API_URL ไม่มี Protoco

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+const getApiUrl = () => {
+    if (process.env.API_URL) {
+        return process.env.API_URL.startsWith('http')
+            ? process.env.API_URL
+            : `http://${process.env.API_URL}`
+    }
+    return 'http://localhost:8080'
+}
+
 const nextConfig = {
     reactStrictMode: true,
     output: 'standalone',
@@ -6,7 +15,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: `${process.env.API_URL || 'http://localhost:8080'}/api/:path*`,
+                destination: `${getApiUrl()}/api/:path*`,
             },
         ]
     },
